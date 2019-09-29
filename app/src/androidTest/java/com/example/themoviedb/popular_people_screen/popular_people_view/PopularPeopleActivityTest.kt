7223ab9aa846f09dcc26dbc10d4dafdc1818eb75
raghda.hany <raghda.hany@ibtikar.net.sa>
2019-09-29 14:07:13 +0200

    UI Test that clicking on item will open the details screen

---
+++ app/src/androidTest/java/com/example/themoviedb/popular_people_screen/popular_people_view/PopularPeopleActivityTest.kt
@@ -0,0 +1,60 @@
+package com.example.themoviedb.popular_people_screen.popular_people_view
+
+
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import android.support.test.espresso.matcher.ViewMatchers.*
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.view.View
+import android.view.ViewGroup
+import com.example.themoviedb.R
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class PopularPeopleActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(PopularPeopleActivity::class.java)
+
+    @Test
+    fun checkDetailsScreenOpened() {
+        val recyclerView = onView(withId(R.id.recyclerViewId))
+
+        val textView = onView(withText("PERSON DETAILS SCREEN"))
+
+        Thread.sleep(3000)
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
+
+        textView.check(matches(withText("PERSON DETAILS SCREEN")))
+    }
+
+    private fun childAtPosition(
+            parentMatcher: Matcher<View>, position: Int): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
