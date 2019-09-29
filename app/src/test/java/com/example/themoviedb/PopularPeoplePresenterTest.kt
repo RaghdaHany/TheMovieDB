3dd989be593b46e6272bde396b46934ae9bf8372
raghda.hany <raghda.hany@ibtikar.net.sa>
2019-09-29 13:39:15 +0200

    unit test changes

--- app/src/test/java/com/example/themoviedb/PopularPeoplePresenterTest.kt
+++ app/src/test/java/com/example/themoviedb/PopularPeoplePresenterTest.kt
@@ -6,11 +6,14 @@ import com.example.themoviedb.popular_people_screen.popular_people_presenter.Pop
 import com.example.themoviedb.popular_people_screen.popular_people_view.PopularPeopleViewInterface
 import com.nhaarman.mockito_kotlin.*
 import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import java.lang.AssertionError
 
 class PopularPeoplePresenterTest {
     internal var popularPeoplePresenter: PopularPeoplePresenter? = null
@@ -26,20 +29,6 @@ class PopularPeoplePresenterTest {
         MockitoAnnotations.initMocks(this)
         popularPeoplePresenter = PopularPeoplePresenter(popularPeopleViewInterface,popularPeopleModelInterface)
     }
-
-//    @Test
-//    fun getFetchedDataTest (){
-//        var page = 1
-//        popularPeoplePresenter?.callFetchingData()
-//
-//        val callback = argumentCaptor<(PopularPeople?) -> Unit>()
-//        val popularPeople = PopularPeople()
-//        Mockito.`when`(popularPeoplePresenter?.callFetchingData())
-//                .then {
-//                    callback.firstValue.invoke(popularPeople)
-//                }
-//        verify(popularPeopleViewInterface).settingAdapterInList()
-//    }
 
     @Test
     fun callScrollFunctionTest() {
@@ -81,19 +70,19 @@ class PopularPeoplePresenterTest {
         assertEquals(expectedSize, sizeAfterClear)
     }
 
-    @Test
-    fun callSwipeFunctionTest() {
-
-        val expectedPageNumber = 1
-        popularPeoplePresenter?.page = 2
-
-        popularPeoplePresenter?.callFetchingData()
-        popularPeoplePresenter?.callSwipeFun()
-
-        this.clearListTest()
-
-        if(popularPeoplePresenter?.searchState != true ){
-            assertEquals(expectedPageNumber, popularPeoplePresenter?.page)
-        }
-    }
+//    @Test
+//    fun callSwipeFunctionTest() {
+//
+//        val expectedPageNumber = 1
+//        var pageNumber = popularPeoplePresenter?.page
+//        pageNumber = 2
+//
+////        popularPeoplePresenter?.callFetchingData()
+//        popularPeoplePresenter?.callSwipeFun()
+//
+////        this.clearListTest()
+//
+////        whenever(popularPeoplePresenter?.searchState != true ).thenReturn(true)
+//            assertEquals(expectedPageNumber, popularPeoplePresenter?.page)
+//    }
 }
